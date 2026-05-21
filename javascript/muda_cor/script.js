590a9a44ac74621ba1fbf46d9bba6031b5e07840
STEFANI FRIAS PIRES <stefani.pires@portalsesisp.org.br>
2026-05-20 22:04:10 -0300

    20/05/2026

---
+++ javascript/muda_cor/script.js
@@ -0,0 +1,15 @@
+// function corVermelha() {
+//     document.body.style.backgroundColor = 'red';
+// }
+
+// function corVerde() {
+//     document.body.style.backgroundColor = 'green';
+// }
+
+// function corAzul() {
+//     document.body.style.backgroundColor = 'blue';
+// }
+
+function mudarCor(cor) {
+    document.body.style.backgroundColor = cor;
+}
